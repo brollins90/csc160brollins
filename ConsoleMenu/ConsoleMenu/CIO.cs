25f4bc8eecdedcfa4cbbda088be23288560e3050
brollins <brollins@293aed11-e72f-ff45-89d2-a28952e9513b>
2014-07-08 22:45:00 +0000

    happy,
    git-svn-id: https://svn.neumont.edu:8443/svn/csc160brollins@8 293aed11-e72f-ff45-89d2-a28952e9513b

---
+++ ConsoleMenu/ConsoleMenu/CIO.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC160_ConsoleMenu
+{
+    public static class CIO
+    {
+        public static T PromptForGeneric<T>(string prompt, Nullable<T> min, Nullable<T> max, Func<string, T> parser) where T : struct, IComparable<T>
+        {
+            Console.WriteLine(prompt);
+            bool isValid = false;
+            T result = default(T);
+
+            while (!isValid)
+            {
+                string input = Console.ReadLine();
+
+                try
+                {
+                    // Will throw an exception if the parse fails, then we only need to check the bounds
+                    result = parser(input);
+                    if (min != null && result.CompareTo((T)min) < 0)
+                    {
+                        Console.WriteLine("Value is too low.  Try again.");
+                    }
+                    else if (max != null && result.CompareTo((T)max) > 0)
+                    {
+                        Console.WriteLine("Value is too high.  Try again.");
+                    }
+                    else
+                    {
+                        isValid = true;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Value is not a valid type.  Try again.");
+                }
+            }
+            return result;
+        }
+
+        public static int PromptForMenuSelection(IEnumerable<string> options, bool withQuit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool PromptForBool(string message)
+        {
+            return PromptForGeneric(message, null, null, Convert.ToBoolean);
+        }
+
+        public static bool PromptForBool(string message, string trueString, string falseString)
+        {
+            //return PromptForGeneric(message, null, null, Convert.ToBoolean);
+            throw new NotImplementedException();
+        }
+
+        public static byte PromptForByte(string message, byte min, byte max)
+        {
+            return PromptForGeneric(message, min, max, Convert.ToByte);
+        }
+
+        public static short PromptForShort(string message, short min, short max)
+        {
+            return PromptForGeneric(message, min, max, Convert.ToInt16);
+        }
+
+        public static int PromptForInt(string message, int min, int max)
+        {
+            return PromptForGeneric(message, min, max, Convert.ToInt32);
+        }
+
+        public static long PromptForLong(string message, long min, long max)
+        {
+            return PromptForGeneric(message, min, max, Convert.ToInt64);
+        }
+
+        public static float PromptForFloat(string message, float min, float max)
+        {
+            return PromptForGeneric(message, min, max, Convert.ToSingle);
+        }
+
+        public static double PromptForDouble(string message, double min, double max)
+        {
+            return PromptForGeneric(message, min, max, Convert.ToDouble);
+        }
+
+        public static decimal PromptForDecimal(string message, decimal min, decimal max)
+        {
+            return PromptForGeneric(message, min, max, Convert.ToDecimal);
+        }
+
+        public static string PromptForInput(string message, bool allowEmpty)
+        {
+            //return PromptForGeneric(message, null, null, Convert.ToString);
+            throw new NotImplementedException();
+        }
+
+        public static char PromptForChar(string message, char min, char max)
+        {
+            return PromptForGeneric(message, min, max, Convert.ToChar);
+        }
+    }
+}
--- ConsoleMenu/ConsoleMenuTest/Program.cs
+++ ConsoleMenu/ConsoleMenuTest/Program.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ConsoleMenu;
+using CSC160_ConsoleMenu;
 
 namespace ConsoleMenuTest
 {
@@ -11,10 +12,49 @@ namespace ConsoleMenuTest
     {
         static void Main(string[] args)
         {
-            bool b = ConsoleMenu.ConsoleMenu.PromptForBool("haha");
-            Console.WriteLine("Bool: " + b);
-            //int i = ConsoleMenu.ConsoleMenu.PromptForGenericWithBounds("hello", 1, 10, Convert.ToInt32);
-            //Console.WriteLine(i);
+            //IEnumerable<string> options = new List<string>() { "Option 1", "Option 2" };
+            //int fromMenuFalse = CSC160_ConsoleMenu.CIO.PromptForMenuSelection(options, false);
+            //Console.WriteLine("fromMenuFalse: " + fromMenuFalse);
+
+            //int fromMenuTrue = CSC160_ConsoleMenu.CIO.PromptForMenuSelection(options, true);
+            //Console.WriteLine("fromMenuTrue: " + fromMenuTrue);
+
+            bool bool1 = CSC160_ConsoleMenu.CIO.PromptForBool("Enter a boolean (Mine).");
+            Console.WriteLine("bool1: " + bool1);
+
+            //bool bool2 = CSC160_ConsoleMenu.CIO.PromptForBool("Enter a boolean.", "true", "false");
+            //Console.WriteLine("bool2: " + bool2);
+
+            byte byte1 = CSC160_ConsoleMenu.CIO.PromptForByte("Enter a byte.", 1, 10);
+            Console.WriteLine("byte1: " + byte1);
+
+            short short1 = CSC160_ConsoleMenu.CIO.PromptForShort("Enter a short.", 1, 10);
+            Console.WriteLine("short1: " + short1);
+
+            int int1 = CSC160_ConsoleMenu.CIO.PromptForInt("Enter a int.", 1, 10);
+            Console.WriteLine("int1: " + int1);
+
+            long long1 = CSC160_ConsoleMenu.CIO.PromptForLong("Enter a long.", 1, 10);
+            Console.WriteLine("long1: " + long1);
+
+            float float1 = CSC160_ConsoleMenu.CIO.PromptForFloat("Enter a float.", 1, 10);
+            Console.WriteLine("float1: " + float1);
+
+            double double1 = CSC160_ConsoleMenu.CIO.PromptForDouble("Enter a double.", 1, 10);
+            Console.WriteLine("double1: " + double1);
+
+            decimal decimal1 = CSC160_ConsoleMenu.CIO.PromptForDecimal("Enter a decimal.", 1, 10);
+            Console.WriteLine("decimal1: " + decimal1);
+
+            //string string1 = CSC160_ConsoleMenu.CIO.PromptForInput("Enter a string (allow empty).", true);
+            //Console.WriteLine("string1: " + string1);
+
+            //string string2 = CSC160_ConsoleMenu.CIO.PromptForInput("Enter a string.", false);
+            //Console.WriteLine("string2: " + string2);
+
+            char char1 = CSC160_ConsoleMenu.CIO.PromptForChar("Enter a char (a-z).", 'a', 'z');
+            Console.WriteLine("char1: " + char1);
+
         }
     }
 }
