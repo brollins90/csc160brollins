fb2e7bc56d7812fbd2587692a20f0728b6dedfe1
brollins <brollins@293aed11-e72f-ff45-89d2-a28952e9513b>
2014-07-09 00:05:39 +0000

    added comments.  done with the CIO lab
    git-svn-id: https://svn.neumont.edu:8443/svn/csc160brollins@10 293aed11-e72f-ff45-89d2-a28952e9513b

--- ConsoleMenu/ConsoleMenu/CIO.cs
+++ ConsoleMenu/ConsoleMenu/CIO.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace CSC160_ConsoleMenu
 {
+    /// <summary>
+    /// The CIO class is used to aid in Console I/O
+    /// Lab 2 in CSC160
+    /// Blake Rollins
+    /// </summary>
     public static class CIO
     {
-
-        public static T PromptForGeneric<T>(string prompt, T? min, T? max, Func<string, T> parser) where T : struct, IComparable<T>
+        /// <summary>
+        /// Will display the prompt at the Console and check for the correct input allowed by the parser function
+        /// </summary>
+        /// <typeparam name="T">The type to return</typeparam>
+        /// <param name="prompt">The question to ask at the Console</param>
+        /// <param name="min">The minimum accepted value (if any)</param>
+        /// <param name="max">The maximum accepted value (if any)</param>
+        /// <param name="parser">The function called to validate the input</param>
+        /// <returns>The value entered at the Console</returns>
+        private static T PromptForGeneric<T>(string prompt, T? min, T? max, Func<string, T> parser) where T : struct, IComparable<T>
         {
             Console.WriteLine(prompt);
             bool isValid = false;
@@ -44,11 +57,23 @@ namespace CSC160_ConsoleMenu
             return result;
         }
 
+        /// <summary>
+        /// Prompts the User for a bool
+        /// </summary>
+        /// <param name="message">The question to ask at the Console</param>
+        /// <returns>The value entered at the Console</returns>
         public static bool PromptForBool(string message)
         {
             return PromptForGeneric(message, null, null, Convert.ToBoolean);
         }
-
+        
+        /// <summary>
+        /// Prompts the User for a bool
+        /// </summary>
+        /// <param name="message">The question to ask at the Console</param>
+        /// <param name="trueString">The string that equates to True</param>
+        /// <param name="falseString">The string that equates to False</param>
+        /// <returns>The value entered at the Console</returns>
         public static bool PromptForBool(string message, string trueString, string falseString)
         {
             Console.WriteLine(message);
@@ -84,31 +109,72 @@ namespace CSC160_ConsoleMenu
             return result;
         }
 
+        /// <summary>
+        /// Prompts the User for a byte
+        /// </summary>
+        /// <param name="message">The question to ask at the Console</param>
+        /// <param name="min">The minimum accepted value (if any)</param>
+        /// <param name="max">The maximum accepted value (if any)</param>
+        /// <returns></returns>
         public static byte PromptForByte(string message, byte min, byte max)
         {
             return PromptForGeneric(message, min, max, Convert.ToByte);
         }
 
+        /// <summary>
+        /// Prompts the User for a char
+        /// </summary>
+        /// <param name="message">The question to ask at the Console</param>
+        /// <param name="min">The minimum accepted value (if any)</param>
+        /// <param name="max">The maximum accepted value (if any)</param>
+        /// <returns>The value entered at the Console</returns>
         public static char PromptForChar(string message, char min, char max)
         {
             return PromptForGeneric(message, min, max, Convert.ToChar);
         }
 
+        /// <summary>
+        /// Prompts the User for a decimal
+        /// </summary>
+        /// <param name="message">The question to ask at the Console</param>
+        /// <param name="min">The minimum accepted value (if any)</param>
+        /// <param name="max">The maximum accepted value (if any)</param>
+        /// <returns>The value entered at the Console</returns>
         public static decimal PromptForDecimal(string message, decimal min, decimal max)
         {
             return PromptForGeneric(message, min, max, Convert.ToDecimal);
         }
 
+        /// <summary>
+        /// Prompts the User for a double
+        /// </summary>
+        /// <param name="message">The question to ask at the Console</param>
+        /// <param name="min">The minimum accepted value (if any)</param>
+        /// <param name="max">The maximum accepted value (if any)</param>
+        /// <returns>The value entered at the Console</returns>
         public static double PromptForDouble(string message, double min, double max)
         {
             return PromptForGeneric(message, min, max, Convert.ToDouble);
         }
 
+        /// <summary>
+        /// Prompts the User for a float
+        /// </summary>
+        /// <param name="message">The question to ask at the Console</param>
+        /// <param name="min">The minimum accepted value (if any)</param>
+        /// <param name="max">The maximum accepted value (if any)</param>
+        /// <returns>The value entered at the Console</returns>
         public static float PromptForFloat(string message, float min, float max)
         {
             return PromptForGeneric(message, min, max, Convert.ToSingle);
         }
 
+        /// <summary>
+        /// Prompts the User for a string
+        /// </summary>
+        /// <param name="message">The question to ask at the Console</param>
+        /// <param name="allowEmpty">If any empty string is acceptable</param>
+        /// <returns>The value entered at the Console</returns>
         public static string PromptForInput(string message, bool allowEmpty)
         {
             Console.WriteLine(message);
@@ -139,16 +205,36 @@ namespace CSC160_ConsoleMenu
             return result;
         }
 
+        /// <summary>
+        /// Prompts the User for a float
+        /// </summary>
+        /// <param name="message">The question to ask at the Console</param>
+        /// <param name="min">The minimum accepted value (if any)</param>
+        /// <param name="max">The maximum accepted value (if any)</param>
+        /// <returns>The value entered at the Console</returns>
         public static int PromptForInt(string message, int min, int max)
         {
             return PromptForGeneric(message, min, max, Convert.ToInt32);
         }
 
+        /// <summary>
+        /// Prompts the User for a long
+        /// </summary>
+        /// <param name="message">The question to ask at the Console</param>
+        /// <param name="min">The minimum accepted value (if any)</param>
+        /// <param name="max">The maximum accepted value (if any)</param>
+        /// <returns>The value entered at the Console</returns>
         public static long PromptForLong(string message, long min, long max)
         {
             return PromptForGeneric(message, min, max, Convert.ToInt64);
         }
 
+        /// <summary>
+        /// Prompts the user to make a choice out of the specified options
+        /// </summary>
+        /// <param name="options">The set of strings that are options</param>
+        /// <param name="withQuit">If option 0 should be set to Quit</param>
+        /// <returns></returns>
         public static int PromptForMenuSelection(IEnumerable<string> options, bool withQuit)
         {
             int minMenuItem = (withQuit) ? 0 : 1;
@@ -166,6 +252,13 @@ namespace CSC160_ConsoleMenu
             return selection;
         }
 
+        /// <summary>
+        /// Prompts the User for a short
+        /// </summary>
+        /// <param name="message">The question to ask at the Console</param>
+        /// <param name="min">The minimum accepted value (if any)</param>
+        /// <param name="max">The maximum accepted value (if any)</param>
+        /// <returns>The value entered at the Console</returns>
         public static short PromptForShort(string message, short min, short max)
         {
             return PromptForGeneric(message, min, max, Convert.ToInt16);
