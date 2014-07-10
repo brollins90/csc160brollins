1ea9bdf75c2fd1f828b26b472273de6e2542365f
brollins <brollins@293aed11-e72f-ff45-89d2-a28952e9513b>
2014-07-10 01:11:57 +0000

    almost
    git-svn-id: https://svn.neumont.edu:8443/svn/csc160brollins@13 293aed11-e72f-ff45-89d2-a28952e9513b

--- ShapeCanvasWPF/ShapeCanvasWPF/MainWindow.xaml.cs
+++ ShapeCanvasWPF/ShapeCanvasWPF/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Automation.Peers;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -12,26 +13,124 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Automation.Provider;
 
 namespace ShapeCanvasWPF
 {
+
+    enum ShapeToCreate
+    {
+        RANDOM, TRIANGLE, RECTANGLE
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        Point currentPoint = new Point();
-        Color currentColor = Color.FromArgb(255, 0, 0, 0);
+        Random _Random = new Random();
+        Point _CurrentPoint = new Point();
+        SolidColorBrush _CurrentColor = new SolidColorBrush(); //.FromArgb(255, 0, 0, 0);
+        ShapeToCreate _NextShapeToCreate;
+        TextBlock _NextShapeText;
 
         public MainWindow()
         {
             InitializeComponent();
+
+            Shape _NextShape = new Rectangle();
+            _NextShape.Width = 30;
+            _NextShape.Height = 30;
+            _NextShape.Stroke = _CurrentColor;
+            _NextShape.Fill = _CurrentColor;
+
+            PlayCanvas.Children.Add(_NextShape);
+            Canvas.SetTop(_NextShape, 10);
+            Canvas.SetLeft(_NextShape, 10);
+
+            _NextShapeText = new TextBlock();
+            _NextShapeText.Text = _NextShapeToCreate.ToString();
+            _NextShapeText.Foreground = new SolidColorBrush(Color.FromArgb(255, 0, 0, 0));
+
+            PlayCanvas.Children.Add(_NextShapeText);
+            Canvas.SetTop(_NextShapeText, 10);
+            Canvas.SetLeft(_NextShapeText, 50);
+
+            PushButtonProgrammatically(buttonColorRandom);
+            PushButtonProgrammatically(buttonShapeRandom);
         }
 
-        private void PlayCanvas_MouseDown_1(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        private void PushButtonProgrammatically(Button buttonToPush)
         {
-            if (e.ButtonState == MouseButtonState.Pressed)
-                currentPoint = e.GetPosition(this);
+            ButtonAutomationPeer peer = new ButtonAutomationPeer(buttonToPush);
+            IInvokeProvider invokeProv = peer.GetPattern(PatternInterface.Invoke) as IInvokeProvider;
+            invokeProv.Invoke();
+        }
+
+        private void SetColor(Color currentColor)
+        {
+            _CurrentColor.Color = currentColor;
+        }
+
+        private void SetShape(ShapeToCreate nextShape)
+        {
+            if (nextShape == ShapeToCreate.RANDOM)
+            {
+                _NextShapeToCreate = (ShapeToCreate)(_Random.Next(0, (Enum.GetNames(typeof(ShapeToCreate)).Length - 1)) + 1);
+            }
+            else
+            {
+                _NextShapeToCreate = nextShape;
+            }
+
+            _NextShapeText.Text = _NextShapeToCreate.ToString();
+        }
+
+        private void ColorButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (e.Source == buttonColorBlue)
+            {
+                SetColor(Color.FromArgb(255, 0, 0, 255));
+            }
+            else if (e.Source == buttonColorGreen)
+            {
+                SetColor(Color.FromArgb(255, 0, 255, 0));
+            }
+            else if (e.Source == buttonColorRed)
+            {
+                SetColor(Color.FromArgb(255, 255, 0, 0));
+            }
+            else if (e.Source == buttonColorRandom)
+            {
+                SetColor(Color.FromArgb(255, (byte)_Random.Next(255), (byte)_Random.Next(255), (byte)_Random.Next(255)));
+            }
+        }
+
+        private void ShapeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (e.Source == buttonShapeRandom)
+            {
+                SetShape(ShapeToCreate.RANDOM);
+            }
+            else if (e.Source == buttonShapeRectangle)
+            {
+                SetShape(ShapeToCreate.RANDOM);
+            }
+            else if (e.Source == buttonShapeTriangle)
+            {
+                SetShape(ShapeToCreate.TRIANGLE);
+            }
+
+        }
+
+        private void PlayCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _CurrentPoint = e.GetPosition(this);
+        }
+
+        private void PlayCanvas_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _CurrentPoint = e.GetPosition(this);
         }
 
         private void PlayCanvas_MouseMove_1(object sender, System.Windows.Input.MouseEventArgs e)
@@ -41,46 +140,17 @@ namespace ShapeCanvasWPF
                 Line line = new Line();
 
                 //line.Stroke = SystemColors.WindowFrameBrush;
-                line.Stroke = new SolidColorBrush(currentColor);
-                line.X1 = currentPoint.X;
-                line.Y1 = currentPoint.Y;
+                line.Stroke = new SolidColorBrush(_CurrentColor.Color);
+                line.X1 = _CurrentPoint.X;
+                line.Y1 = _CurrentPoint.Y;
                 line.X2 = e.GetPosition(this).X;
                 line.Y2 = e.GetPosition(this).Y;
 
-                currentPoint = e.GetPosition(this);
+                _CurrentPoint = e.GetPosition(this);
 
                 PlayCanvas.Children.Add(line);
             }
         }
 
-        private void buttonShapeRandom_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
-        private void buttonShapeTriangle_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void buttonShapeRectangle_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void buttonColorRed_Click(object sender, RoutedEventArgs e)
-        {
-            currentColor = Color.FromArgb(255, 255, 0, 0);
-        }
-
-        private void buttonColorGreen_Click(object sender, RoutedEventArgs e)
-        {
-            currentColor = Color.FromArgb(255, 0, 255, 0);
-        }
-
-        private void buttonColorBlue_Click(object sender, RoutedEventArgs e)
-        {
-            currentColor = Color.FromArgb(255, 0, 0, 255);
-        }
     }
 }
