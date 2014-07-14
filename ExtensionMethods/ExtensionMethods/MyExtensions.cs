f357d09eaf8210f16a2e17caab2c4d100900a566
brollins <brollins@293aed11-e72f-ff45-89d2-a28952e9513b>
2014-07-14 22:01:40 +0000

    one extension method down.
    git-svn-id: https://svn.neumont.edu:8443/svn/csc160brollins@18 293aed11-e72f-ff45-89d2-a28952e9513b

---
+++ ExtensionMethods/ExtensionMethods/MyExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public static class MyExtensions
+    {
+        public static void P(this IEnumerable<object> ien) {
+            String output = "";
+            foreach(object o in ien) {
+                output += o.ToString();
+                output += ", ";
+            }
+            Console.WriteLine(output.Substring(0,output.Length - 2));
+        }
+
+        public static int ToPower(this int x, int y) {
+            return (int)Math.Pow(x, y);
+        }
+    }
+}
---
+++ ExtensionMethods/ExtensionMethodsTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExtensionMethods;
+
+namespace ExtensionMethodsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i1 = 5.ToPower(2);
+            Console.WriteLine(i1);
+        }
+    }
+}
