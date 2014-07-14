380be0f6a2eca5dc1fab5ecb080c8a618c6d926c
brollins <brollins@293aed11-e72f-ff45-89d2-a28952e9513b>
2014-07-14 23:31:02 +0000

    extension lab complete
    git-svn-id: https://svn.neumont.edu:8443/svn/csc160brollins@20 293aed11-e72f-ff45-89d2-a28952e9513b

--- ExtensionMethods/ExtensionMethods/MyExtensions.cs
+++ ExtensionMethods/ExtensionMethods/MyExtensions.cs
@@ -8,17 +8,81 @@ namespace ExtensionMethods
 {
     public static class MyExtensions
     {
-        public static void P(this IEnumerable<object> ien) {
+        /// <summary>
+        /// Returns a bool if the int is even
+        /// </summary>
+        /// <param name="i">The int to check for eveness</param>
+        /// <returns>If the int is even.</returns>
+        public static bool IsEven(this int i)
+        {
+            return i % 2 == 0;
+        }
+
+        /// <summary>
+        /// Returns a bool signifying whether the string is a palindrome 
+        /// </summary>
+        /// <param name="s">The string to test for palendronism.</param>
+        /// <returns>A bool signifying whether the string is a palindrome.</returns>
+        public static bool IsPalindrome(this string s)
+        {
+            int i = 0;
+            char[] sArray = s.ToCharArray();
+            for (int j = s.Length - 1; i < j; j--)
+            {
+                if (sArray[i] != sArray[j])
+                {
+                    return false;
+                }
+                i++;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Prints out the objects in any collection, separating each value with a comma.
+        /// </summary>
+        /// <param name="ien">The enumerable set of objects to be printed.</param>
+        public static void P(this IEnumerable<object> ien)
+        {
             String output = "";
-            foreach(object o in ien) {
+            foreach (object o in ien)
+            {
                 output += o.ToString();
                 output += ", ";
             }
-            Console.WriteLine(output.Substring(0,output.Length - 2));
+            Console.WriteLine(output.Substring(0, output.Length - 2));
         }
 
-        public static int ToPower(this int x, int y) {
-            return (int)Math.Pow(x, y);
+        /// <summary>
+        /// Returns a reversed copy of the string.
+        /// </summary>
+        /// <param name="s">The string to reverse</param>
+        /// <returns>A string that a reverse copy</returns>
+        public static string Reverse(this string s)
+        {
+            char[] chars = s.ToCharArray();
+            Array.Reverse(chars);
+            return new string(chars);
+        }
+
+        /// <summary>
+        /// Returns the value of the original int raised to the power of the exponent parameter
+        /// </summary>
+        /// <param name="integer"></param>
+        /// <param name="exponent"></param>
+        /// <returns>The value of the int raised to the specified power.</returns>
+        public static int ToPower(this int integer, int exponent) {
+            return (int)Math.Pow(integer, exponent);
+        }
+
+        /// <summary>
+        /// Converts a string to an int
+        /// </summary>
+        /// <param name="s">The string to convert</param>
+        /// <returns>The int that is represented in the string</returns>
+        public static int ToInt(this string s)
+        {
+            return Convert.ToInt32(s);
         }
     }
 }
--- ExtensionMethods/ExtensionMethodsTest/Program.cs
+++ ExtensionMethods/ExtensionMethodsTest/Program.cs
@@ -9,10 +9,53 @@ namespace ExtensionMethodsTest
 {
     class Program
     {
+        /// <summary>
+        /// Some tests for the Extension methods
+        /// 
+        /// Blake Rollins
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            int i1 = 5.ToPower(2);
-            Console.WriteLine(i1);
+            Console.WriteLine("An example of a collection being printed as a comma delimited string:");
+            List<string> l1 = new List<string>() {
+                "one",
+                "two",
+                "three"
+            };
+            l1.P();
+            Console.WriteLine("");
+
+            Console.WriteLine("An example of an int raised to a power:");
+            int intToPower = 5.ToPower(2);
+            Console.WriteLine("{0} raised to {1}: {2}",5, 2, intToPower);
+            Console.WriteLine("");
+
+            Console.WriteLine("An example of a palindrome string:");
+            string palindromeString = "bob";
+            Console.WriteLine("'{0}' is palindrome: {1}", palindromeString, palindromeString.IsPalindrome());
+            Console.WriteLine("");
+            
+            Console.WriteLine("An example of a non-palindrome string:");
+            string nonPalindromeString = "bob2";
+            Console.WriteLine("'{0}' is palindrome: {1}", nonPalindromeString, nonPalindromeString.IsPalindrome());
+            Console.WriteLine("");
+
+            Console.WriteLine("An example of a covnerting a string to an int:");
+            string intString = "5";
+            Console.WriteLine("{0} is {1}", intString, intString.ToInt());
+            Console.WriteLine("");
+
+            Console.WriteLine("An example of a reversing a string:");
+            string rev = "reversie";
+            Console.WriteLine("{0} reversed: {1}", rev, rev.Reverse());
+            Console.WriteLine("");
+
+            Console.WriteLine("An example of a checking an int for evenness and oddness:");
+            Console.WriteLine("2: {0}", 2.IsEven());
+            Console.WriteLine("15: {0}", 15.IsEven());
+
         }
     }
 }
