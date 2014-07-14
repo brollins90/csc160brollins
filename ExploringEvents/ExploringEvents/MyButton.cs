d9faf9140f819f8a6d912a9f86c9e034dc3af573
brollins <brollins@293aed11-e72f-ff45-89d2-a28952e9513b>
2014-07-14 22:40:58 +0000

    done with the event lab
    git-svn-id: https://svn.neumont.edu:8443/svn/csc160brollins@19 293aed11-e72f-ff45-89d2-a28952e9513b

---
+++ ExploringEvents/ExploringEvents/MyButton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExploringEvents
+{
+    /// <summary>
+    /// Test button to practice delegates
+    /// </summary>
+    class MyButton
+    {
+        /// <summary>
+        /// The Click event
+        /// </summary>
+        public MyClickDelegate ClickEvent;
+
+        /// <summary>
+        /// The Button name
+        /// </summary>
+        public string ButtonName { get; private set; }
+
+        public MyButton(string buttonName)
+        {
+            ButtonName = buttonName;
+        }
+
+        /// <summary>
+        /// Event that "clicks" the button
+        /// </summary>
+        public void ClickButton()
+        {
+            ClickEvent(ButtonName);
+        }
+    }
+}
---
+++ ExploringEvents/ExploringEvents/MyView.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSC160_ConsoleMenu;
+
+
+namespace ExploringEvents
+{
+    public delegate void MyClickDelegate(string s);
+
+    /// <summary>
+    /// This program lets us practice using delegates as event handlers.
+    /// Prompts the user at the console to "click" a button
+    /// 
+    /// Blake Rollins
+    /// 
+    /// </summary>
+    class MyView
+    {
+
+
+        /// <summary>
+        /// Code to handle a button click
+        /// </summary>
+        /// <param name="s">The button name</param>
+        public void ButtonClick_Handler(string s)
+        {
+            Console.WriteLine("{0} was clicked", s);
+        }
+
+        /// <summary>
+        /// Go!
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            MyView v1 = new MyView();
+            MyButton b1 = new MyButton("OK");
+            MyButton b2 = new MyButton("Cancel");
+            MyButton b3 = new MyButton("Bob");
+            b1.ClickEvent += v1.ButtonClick_Handler;
+            b2.ClickEvent += v1.ButtonClick_Handler;
+            b3.ClickEvent += v1.ButtonClick_Handler;
+
+            List<string> options = new List<string>() {
+                "Click OK",
+                "Click Cancel",
+                "Click Boc",
+                "Click All"
+            };
+
+            bool running = true;
+            while (running)
+            {
+                int selection = CSC160_ConsoleMenu.CIO.PromptForMenuSelection(options, true);
+
+                switch (selection)
+                {
+                    case 0:
+                        running = false;
+                        break;
+                    case 1:
+                        b1.ClickButton();
+                        break;
+                    case 2:
+                        b2.ClickButton();
+                        break;
+                    case 3:
+                        b3.ClickButton();
+                        break;
+                    case 4:
+                        b1.ClickButton();
+                        b2.ClickButton();
+                        b3.ClickButton();
+                        break;
+                }
+            }
+        }
+    }
+}
