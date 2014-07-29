39d13bc3d1e6f6ac3097777d1338b2c4b6ac322a
brollins <brollins@293aed11-e72f-ff45-89d2-a28952e9513b>
2014-07-29 00:20:26 +0000

    maybe better
    git-svn-id: https://svn.neumont.edu:8443/svn/csc160brollins@34 293aed11-e72f-ff45-89d2-a28952e9513b

--- GameOfLife/GameOfLife/WindowOfLife.xaml.cs
+++ GameOfLife/GameOfLife/WindowOfLife.xaml.cs
@@ -13,15 +13,90 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Threading;
+using System.Numerics;
+using System.Windows.Threading;
 //using System.Windows.Automation.Provider;
 
 namespace GameOfLife
 {
+
+    public static class ExtensionMethods {
+
+        public static void SetBit(this BigInteger value, int bit, out BigInteger o)
+        {
+            //Console.WriteLine("org  :   " + value.ToBinaryString());
+            //BigInteger mask = BigInteger.One;
+            //Console.WriteLine("mask1:       " + mask.ToBinaryString());
+            //mask = mask << bit;
+            //Console.WriteLine("mask2:     " + mask.ToBinaryString());
+            //BigInteger notMask = ~mask;
+            //Console.WriteLine("mask~: " + notMask.ToBinaryString());
+            ////decimal mask = (decimal)1 << bit;
+            //BigInteger b2 = value | mask;
+            //Console.WriteLine("b2   :   " + b2.ToBinaryString());
+            //o = b2;
+            ////BigInteger mask = BigInteger.Parse("1") << bit;
+            ////value |= mask;
+
+            BigInteger mask = BigInteger.Parse("1") << bit;
+            o = value | mask;            
+        }
+
+        public static void UnsetBit(this BigInteger value, int bit, out BigInteger o)
+        {
+            BigInteger mask = BigInteger.Parse("1") << bit;
+            o = value & ~mask;
+        }
+
+        public static bool IsSet(this BigInteger value, int bit)
+        {
+            BigInteger i = value & (BigInteger.One << bit);
+            //Console.WriteLine(i.ToBinaryString());
+            return i != 0; ;
+        }
+
+        public static string ToBinaryString(this BigInteger bigint)
+        {
+            var bytes = bigint.ToByteArray();
+            var idx = bytes.Length - 1;
+
+            // Create a StringBuilder having appropriate capacity.
+            var base2 = new StringBuilder(bytes.Length * 8);
+
+            // Convert first byte to binary.
+            var binary = Convert.ToString(bytes[idx], 2);
+
+            // Ensure leading zero exists if value is positive.
+            if (binary[0] != '0' && bigint.Sign == 1)
+            {
+                base2.Append('0');
+            }
+
+            // Append binary string to StringBuilder.
+            base2.Append(binary);
+
+            // Convert remaining bytes adding leading zeros.
+            for (idx--; idx >= 0; idx--)
+            {
+                base2.Append(Convert.ToString(bytes[idx], 2).PadLeft(8, '0'));
+            }
+
+            return base2.ToString();
+        }
+    }
+
     /// <summary>
     /// Interaction logic for WindowOfLife.xaml
     /// </summary>
     public partial class WindowOfLife : Window
     {
+
+        public BigInteger[] GameRows;
+        public BigInteger[] GameRowsThis;
+
+        private DispatcherTimer Timer;
+
+
         public int NumberOfRows { get; set; }
         public int NumberOfColumns { get; set; }
         private Cell[,] cells;
@@ -29,63 +104,17 @@ namespace GameOfLife
         private Grid GameGrid;
 
 
-
         public WindowOfLife()
         {
-            //NumberOfColumns = 10;
-            //NumberOfRows = 10;
-            //int counter = 0;
-            //int onCount = 4;
-            //cells = new Cell[NumberOfColumns,NumberOfRows];
-            //cellsThisRound = new bool[NumberOfColumns, NumberOfRows];
-
             InitializeComponent();
+            Timer = new DispatcherTimer();
+            Timer.Interval = TimeSpan.FromSeconds(1);
+            Timer.Tick += Timer_Tick;
+        }
 
-            //Button b1 = new Button();
-            //b1.Click += ProceedButton_Click;
-            //b1.Height = 50;
-            //b1.Content = "push";
-            //GameControls.Children.Add(b1);
-
-
-            //GameGrid = new Grid();
-            //GameGrid.ShowGridLines = true;
-
-            //for (int i = 0; i < NumberOfColumns; i++)
-            //{
-            //    GameGrid.ColumnDefinitions.Add(new ColumnDefinition());
-            //}
-            //for (int i = 0; i < NumberOfRows; i++)
-            //{
-            //    GameGrid.RowDefinitions.Add(new RowDefinition());
-            //}
-            //for (int colIndex = 0; colIndex < NumberOfColumns; colIndex++)
-            //{
-            //    for (int rowIndex = 0; rowIndex < NumberOfRows; rowIndex++)
-            //    {
-            //        Label tempLabel = new Label();
-            //        Cell tempCell = new Cell();
-            //        tempCell.Alive = (counter++ % onCount) == 0 ? true : false;
-            //        tempLabel.DataContext = tempCell;
-            //        tempLabel.Content = string.Format("{0},{1}", colIndex,rowIndex);
-            //        tempLabel.MouseLeftButtonDown += Cell_Click;
-
-            //        var bind = new Binding("Alive")
-            //        {
-            //            Converter = new AliveToColorConverter()
-            //        };
-
-            //        tempLabel.SetBinding(Label.BackgroundProperty, bind);
-
-            //        GameGrid.Children.Add(tempLabel);
-            //        Grid.SetColumn(tempLabel, colIndex);
-            //        Grid.SetRow(tempLabel, rowIndex);
-            //        cells[colIndex,rowIndex] = tempCell;
-            //        //Console.WriteLine("{0}: - {1}", counter, tempCell.Alive);
-            //    }
-            //}
-            //GameFrame.Children.Add(GameGrid);
-            //this.Content = GameFrame;
+        void Timer_Tick(object sender, EventArgs e)
+        {
+            ProcessStep();
         }
         
         public Cell GetCellAt(int col, int row)
@@ -99,9 +128,84 @@ namespace GameOfLife
 
         public void ProcessStep()
         {
-            for (int i = 0; i < NumberOfColumns; i++) {
-                for (int j = 0; j < NumberOfRows; j++) {
-                    cellsThisRound[i,j] = cells[i,j].Alive;
+            for (int rowIndex = 0; rowIndex < NumberOfRows; rowIndex++)
+            {
+                for (int colIndex = 0; colIndex < NumberOfColumns; colIndex++)
+                {
+                    if (cells[rowIndex, colIndex].Alive)
+                    {
+                        GameRows[rowIndex].SetBit(colIndex, out GameRows[rowIndex]);
+                    }
+                    else
+                    {
+                        GameRows[rowIndex].UnsetBit(colIndex, out GameRows[rowIndex]);
+                    }
+                }
+                GameRowsThis[rowIndex] = BigInteger.Parse(GameRows[rowIndex].ToString());
+            }
+
+            //GameRowsThis = GameRows;
+
+            for (int rowIndex = 0; rowIndex < NumberOfRows; rowIndex++)
+            {
+                //Console.WriteLine(GameRows[rowIndex].ToBinaryString());
+                int rowAbove = rowIndex - 1;
+                int rowBelow = rowIndex + 1;
+
+                for (int colIndex = 0; colIndex < NumberOfColumns; colIndex++)
+                {
+                    int left = colIndex - 1;
+                    int right = colIndex + 1;
+
+                    int nCount = 0;
+                    if (rowAbove > -1 && GameRows[rowAbove].IsSet(left))
+                        nCount++;
+                    if (rowAbove > -1 && GameRows[rowAbove].IsSet(colIndex))
+                        nCount++;
+                    if (rowAbove > -1 && GameRows[rowAbove].IsSet(right))
+                        nCount++;
+                    if (GameRows[rowIndex].IsSet(right))
+                        nCount++;
+                    if (rowBelow < NumberOfRows && GameRows[rowBelow].IsSet(right))
+                        nCount++;
+                    if (rowBelow < NumberOfRows && GameRows[rowBelow].IsSet(colIndex))
+                        nCount++;
+                    if (rowBelow < NumberOfRows && GameRows[rowBelow].IsSet(left))
+                        nCount++;
+                    if (GameRows[rowIndex].IsSet(left))
+                        nCount++;
+
+                    //Console.WriteLine("{0},{1}: {2}", rowIndex, colIndex, nCount);
+                    switch (nCount)
+                    {
+                        default:
+                            GameRowsThis[rowIndex].UnsetBit(colIndex, out GameRowsThis[rowIndex]);
+                            cells[rowIndex, colIndex].Alive = false;
+                            break;
+                        case 2:
+                            if (cells[rowIndex, colIndex].Alive)
+                            {
+                                GameRowsThis[rowIndex].SetBit(colIndex, out GameRowsThis[rowIndex]);
+                                cells[rowIndex, colIndex].Alive = true;
+                            }
+                            break;
+                        case 3:
+                            GameRowsThis[rowIndex].SetBit(colIndex, out GameRowsThis[rowIndex]);
+                            cells[rowIndex, colIndex].Alive = true;
+                            break;
+
+                    }
+                }
+            }
+        }
+
+        public void ProcessStepOld()
+        {
+            for (int i = 0; i < NumberOfColumns; i++)
+            {
+                for (int j = 0; j < NumberOfRows; j++)
+                {
+                    cellsThisRound[i, j] = cells[i, j].Alive;
                 }
             }
 
@@ -118,7 +222,7 @@ namespace GameOfLife
                     neighbors[5] = GetCellAt(colIndex - 0, rowIndex + 1);
                     neighbors[6] = GetCellAt(colIndex - 1, rowIndex + 1);
                     neighbors[7] = GetCellAt(colIndex - 1, rowIndex - 0);
-                    
+
                     int nCount = 0;
                     for (int nIndex = 0; nIndex < 8; nIndex++)
                     {
@@ -128,23 +232,24 @@ namespace GameOfLife
                             nCount++;
                         }
                     }
-                    Console.WriteLine("{0},{1}: {2}",colIndex, rowIndex, nCount);
+                    //Console.WriteLine("{0},{1}: {2}", colIndex, rowIndex, nCount);
 
                     switch (nCount)
                     {
                         default:
-                            cellsThisRound[colIndex,rowIndex] = false;
+                            cellsThisRound[colIndex, rowIndex] = false;
                             break;
                         case 2:
-                            if (cells[colIndex,rowIndex].Alive) {
-                                cellsThisRound[colIndex,rowIndex] = true;
+                            if (cells[colIndex, rowIndex].Alive)
+                            {
+                                cellsThisRound[colIndex, rowIndex] = true;
                             }
                             break;
                         case 3:
-                            cellsThisRound[colIndex,rowIndex] = true;
+                            cellsThisRound[colIndex, rowIndex] = true;
                             break;
                     }
-                    
+
                 }
             }
 
@@ -178,29 +283,31 @@ namespace GameOfLife
             int counter = 0;
             int onCount = 4;
 
-            cells = new Cell[NumberOfColumns, NumberOfRows];
-            cellsThisRound = new bool[NumberOfColumns, NumberOfRows];
+            cells = new Cell[NumberOfRows, NumberOfColumns];
+            cellsThisRound = new bool[NumberOfRows, NumberOfColumns];
+            GameRows = new BigInteger[NumberOfRows];
+            GameRowsThis = new BigInteger[NumberOfRows];
 
             GameGrid = new Grid();
             GameGrid.ShowGridLines = true;
 
-            for (int i = 0; i < NumberOfColumns; i++)
-            {
-                GameGrid.ColumnDefinitions.Add(new ColumnDefinition());
-            }
             for (int i = 0; i < NumberOfRows; i++)
             {
                 GameGrid.RowDefinitions.Add(new RowDefinition());
             }
-            for (int colIndex = 0; colIndex < NumberOfColumns; colIndex++)
+            for (int i = 0; i < NumberOfColumns; i++)
             {
-                for (int rowIndex = 0; rowIndex < NumberOfRows; rowIndex++)
+                GameGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+            for (int rowIndex = 0; rowIndex < NumberOfRows; rowIndex++) 
+            {
+                for (int colIndex = 0; colIndex < NumberOfColumns; colIndex++)
                 {
                     Label tempLabel = new Label();
                     Cell tempCell = new Cell();
                     tempCell.Alive = (counter++ % onCount) == 0 ? true : false;
                     tempLabel.DataContext = tempCell;
-                    tempLabel.Content = string.Format("{0},{1}", colIndex, rowIndex);
+                    tempLabel.Content = string.Format("{0},{1}", rowIndex, colIndex);
                     tempLabel.MouseLeftButtonDown += Cell_Click;
 
                     var bind = new Binding("Alive")
@@ -211,13 +318,24 @@ namespace GameOfLife
                     tempLabel.SetBinding(Label.BackgroundProperty, bind);
 
                     GameGrid.Children.Add(tempLabel);
-                    Grid.SetColumn(tempLabel, colIndex);
                     Grid.SetRow(tempLabel, rowIndex);
-                    cells[colIndex, rowIndex] = tempCell;
+                    Grid.SetColumn(tempLabel, colIndex);
+                    cells[rowIndex, colIndex] = tempCell;
                     //Console.WriteLine("{0}: - {1}", counter, tempCell.Alive);
                 }
             }
             GameFrame.Children.Add(GameGrid);
+        }
+
+        private void StartTimerButton_Click(object sender, RoutedEventArgs e)
+        {
+            Timer.Interval = TimeSpan.FromMilliseconds((1000 / TimeSlider.Value));
+            Timer.Start();
+        }
+
+        private void StopTimerButton_Click(object sender, RoutedEventArgs e)
+        {
+            Timer.Stop();
         }
 
 
